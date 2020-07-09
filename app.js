c712882322626b0b7cd3e88e9e51e5c507c2ba48
DrBretto <59483898+DrBretto@users.noreply.github.com>
2020-07-09 11:29:37 -0400

    updates

--- app.js
+++ app.js
@@ -1,61 +1,70 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
 
-  questions: [  //theme: geography
-    {
-      question: 'Which if the following is a country?',
-      answers: [
-        'Illinois',
-        'Africa',
-        'Canada',
-        'Fred'
-      ],
-      correctAnswer: 'Canada'
-    },
-    {
-      question: 'What is the capital of New Hampshire?',
-      answers: [
-        'Manchester',
-        'Nashua',
-        'Concord',
-        'Istanbul'
-      ],
-      correctAnswer: 'Concord'
-    },
-    {
-      question: 'Which of these is a country AND a continent?',
-      answers: [
-        'Australia',
-        'Antarctica',
-        'North America',
-        'South America'
-      ],
-      correctAnswer: 'Australia'
-    },
-    {
-      question: 'Which Country is the largest? (area)',
-      answers: [
-        'USA',
-        'Canada',
-        'Russia',
-        'India'
-      ],
-      correctAnswer: 'Russia'
-    },
-    {
-      question: 'Which country has the largest population?',
-      answers: [
-        'India',
-        'Russia',
-        'Japan',
-        'China'
-      ],
-      correctAnswer: 'China'
-    },
-  ],
+let questions = [  //theme: geography
+  { //welcome screen
+    image: "image/welcome.jpg",
+    question: "Welcome",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    buttonText: "Start Game!"
+
+  },
+  { //question 1
+    image: "image1.jpg",
+    question: 'Which if the following is a country?',
+    answers: [
+      'Illinois',
+      'Africa',
+      'Canada',
+      'Fred'
+    ],
+    correctAnswer: 'Canada',
+    buttonText: "Subit"
+  },
+  {
+    question: 'What is the capital of New Hampshire?',
+    answers: [
+      'Manchester',
+      'Nashua',
+      'Concord',
+      'Istanbul'
+    ],
+    correctAnswer: 'Concord'
+  },
+  {
+    question: 'Which of these is a country AND a continent?',
+    answers: [
+      'Australia',
+      'Antarctica',
+      'North America',
+      'South America'
+    ],
+    correctAnswer: 'Australia'
+  },
+  {
+    question: 'Which Country is the largest? (area)',
+    answers: [
+      'USA',
+      'Canada',
+      'Russia',
+      'India'
+    ],
+    correctAnswer: 'Russia'
+  },
+  {
+    question: 'Which country has the largest population?',
+    answers: [
+      'India',
+      'Russia',
+      'Japan',
+      'China'
+    ],
+    correctAnswer: 'China'
+  },
+];
+
+
+
+const STORE = {
   quizStarted: false,
   questionNumber: 0,
   score: 0
@@ -63,20 +72,81 @@ const store = {
 
 
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+
+function main() {
+
+  renderScreen(welcome);
+
+}
+
+
+let welcome = `
+<div class="box">
+  <div class="stats">
+    <p>Question# 1 of 5</p>
+    <p>Score: 4 right, 3 wrong</p>
+  </div>
+  <div class="box"><img height = 200px width = 200px src="${questions[0].image}"></div>
+  <div class="questions">questions</div>
+  <div class="answers">answers</div>
+  <div>
+    <button class="submit">Start Game!
+    </button>
+  </div>
+
+</div>
+`;
+
+function renderScreen(screenType) {
+  $("h1").html(`
+  <div>
+  Our Game
+  </div>`)
+  $("main").html(screenType);
+
+}
+
+$(main)
+/*
+
+function QuestionScreen
+
+  function renderQuestions
+
+  *multiple choice question  ---- keyboard accessible
+    *A
+    *B
+    *C
+    *D
+
+  * submit button
+
+  function quizGrader --- intermediate determines question correctness
+    * correct answer --- "Congrats"
+    * incorrect answer -- "Sorry - the correct answer was X"
+  * pulling from a correctAnswers array
+
+
+  function runningScore  - desplayed on all question screens
+   * correct answers / index#
+
+
+  function endScreen
+  * final score
+  * "congrats yada yada"
+  * play again button
+
+
+** Variables ***********
+
+Array of Question Objects  --- do we build a constructor like with LOTR?
+
+
+currentQuestion
+
+currentScore
+
+  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
@@ -93,76 +163,17 @@ const store = {
 
 /* HTML WireFrame */
 
-function main(){
-
-  renderScreen(welcome);
-
-}
-
-let welcome = `
-<div class="box">
-  <div class="stats">
-    <p>Question# 1 of 5</p>
-    <p>Score: 4 right, 3 wrong</p>
-  </div>
-  <div class="box"><img height = 200px width = 200px src="images/welcome.jpg"></div>
-  <div class="questions">questions</div>
-  <div class="answers">answers</div>
-  <div>
-    <button class="submit">Start Game!
-    </button>
-  </div>
-
-</div>
-`;
-
-function renderScreen(screenType){
-  $("h1").html(`
-  <div>
-  Our Game
-  </div>`)
-  $("main").html(screenType);
-
-}
-
-$(main)
-/*
-
-function QuestionScreen
-
-  function renderQuestions
-
-  *multiple choice question  ---- keyboard accessible 
-    *A
-    *B
-    *C
-    *D
-
-  * submit button 
-
-  function quizGrader --- intermediate determines question correctness
-    * correct answer --- "Congrats"
-    * incorrect answer -- "Sorry - the correct answer was X"
-  * pulling from a correctAnswers array
-
-
-  function runningScore  - desplayed on all question screens
-   * correct answers / index#
-
-
-  function endScreen
-  * final score
-  * "congrats yada yada"
-  * play again button
-  
-  
-** Variables ***********
-  
-Array of Question Objects  --- do we build a constructor like with LOTR?
-
-
-currentQuestion
-
-currentScore
-
-  */
+/**
+ *
+ * Technical requirements:
+ *
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
+ * See your course material, consult your instructor, and reference the slides for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ *
+ */
